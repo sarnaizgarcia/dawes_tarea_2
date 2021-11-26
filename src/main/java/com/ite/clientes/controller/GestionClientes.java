c51b99b7fe7d1260268082418a66d07a6da82c74
Silvia A G <sarnaizgarcia@gmail.com>
2021-11-26 23:40:24 +0100

    borrador listado eventos activos

--- src/main/java/com/ite/clientes/controller/GestionClientes.java
+++ src/main/java/com/ite/clientes/controller/GestionClientes.java
@@ -1,10 +1,14 @@
 package com.ite.clientes.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ite.clientes.modelo.beans.Evento;
 import com.ite.clientes.modelo.repository.IntCliente;
 import com.ite.clientes.modelo.repository.IntEvento;
 
@@ -14,13 +18,24 @@ public class GestionClientes {
 	
 	@Autowired
 	private IntCliente iCliente;
-//	@Autowired
-//	private IntEvento iEvento;
+	@Autowired
+	private IntEvento iEvento;
 	
 	@GetMapping("/login")
 	public String acceso() {
 		System.out.println(iCliente.findAll().toString());
 		return "login";
+	}
+	
+	@GetMapping("/activos")
+	public String mostrarActivos(Model model) {
+		System.out.println("EO");
+
+		List<Evento> eventos = iEvento.findAll();
+		System.out.println(eventos);
+		model.addAttribute("eventos", eventos);
+		// Pintar los eventos activos en la vista activos
+		return "activos";
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/vistas/activos.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Tarea 2 - DAWES</title>
+</head>
+<body>
+	${eventos}
+	
+</body>
+</html>
